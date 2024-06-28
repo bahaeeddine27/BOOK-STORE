ad5dbb697fd14bd107a391a5ee1633f8287f6e23
Bahaeeddine27 <bahaeeddineabbassi@gmail.com>
2024-06-28 16:45:39 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3000;
 
-export const mongoDBURL = 'mongodb+srv://root:root@books-store-mern.cswemxg.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+export const mongoDBURL = 'MongodbURL'
